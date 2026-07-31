aa6e95524df2128565df8e2d5c116430c67697f9
Ana Ledier <ana.passo@portalsesisp.org.br>
2026-07-31 15:03:05 -0300

    feat: adiciona atividade minha estante

---
+++ 01-ambiente-node/Atividade 01/estante.js
@@ -0,0 +1,16 @@
+// ===== LIVRO 1 =====
+const livro1 = {
+titulo: "Clean Code",
+autor: "Robert C. Martin",
+preco: 89.9,
+estoque: 12,
+};
+// ===== LIVRO 2 — TROQUE os quatro valores abaixo =====
+const livro2 = {
+titulo: "Melhor do que nos filmes",
+autor: "Lynn Painter",
+preco: 49.78,
+estoque: 15,
+};
+// Esta linha entrega os dois livros para os outros arquivos usarem
+module.exports = { livro1, livro2 };
